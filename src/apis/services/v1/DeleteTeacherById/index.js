520243b61cec850f2997160c51f3300562d7b524
amit00986 <aryan.amit0824@gmail.com>
2023-09-27 20:05:21 +0530

    delete soft api

--- src/apis/services/v1/DeleteTeacherById/index.js
+++ src/apis/services/v1/DeleteTeacherById/index.js
@@ -3,7 +3,7 @@ const TeacherData = require('@models/Teacher');
 // Service function to delete a teacher by ID
 const deleteTeacherById = async (teacherId) => {
     try {
-      const deletedTeacher = await TeacherData.findByIdAndDelete(teacherId);
+      const deletedTeacher = await TeacherData.findByIdAndUpdate(teacherId);
       return {};
     } catch (error) {
       throw new Error('Failed to delete teacher');
