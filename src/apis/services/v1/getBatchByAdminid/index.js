661dcfa49a0bd1e04ce66a1a639e45d1cea22912
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-02 14:01:18 +0530

    Create index.js

---
+++ src/apis/services/v1/getBatchByAdminid/index.js
@@ -0,0 +1,20 @@
+const BatchData = require('@models/Batch');
+// Service function to get  find batch by teacher ID
+const getBatchAdminid = async (userId) => {
+  try {
+    const batch = await BatchData.find({ createruserId: userId });
+    
+    if (batch === null) {
+      return {
+        status: 404,
+        message: 'BATCH_NOT_FOUND_FOR_TEACHER',
+      };
+    }
+    return batch;
+  } catch (error) {
+    throw new Error('Failed to get Batch By TeacherId');
+  }
+};
+module.exports = {
+  getBatchAdminid
+}  
