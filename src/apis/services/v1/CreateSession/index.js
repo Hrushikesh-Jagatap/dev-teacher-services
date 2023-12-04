2752acd0ae6dc8654afa44514609965fb60c3dcd
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-04 10:17:13 +0530

    Update index.js

--- src/apis/services/v1/CreateSession/index.js
+++ src/apis/services/v1/CreateSession/index.js
@@ -1,33 +1,37 @@
 const sessionDb = require('@models/Session');
 const { createUUID } = require('@common/libs/UUID/UUIDV4');
-const { addSessionToChapter } = require('@services/v1/updateChapter')
+const { addSessionToChapter } = require('@services/v1/updateChapter');
 
-
-// Service function to create a new session in  chapter
+// Service function to create a new session in a chapter
 const createSession = async (sessionData) => {
     try {
-
         const session_id = `SESSION_${createUUID()}`;
-
         sessionData.session_id = session_id;
-
         const { chapter_id } = sessionData;
+        const updateChapter = await addSessionToChapter(chapter_id, session_id);
 
-        const updateChapter = await addSessionToChapter(chapter_id, session_id)
-
-        if (updateChapter === 'chapter not found') {
+        if (updateChapter === 'Chapter not found') {
             return {
                 status: 404,
                 message: 'Error while updating sessionId in chapter',
             };
         }
 
+        const mettingUrl = `https://myeduc.ddns.net/${session_id}`;
+
+        // Add the meetingUrl to the sessionData
+        sessionData.mettingUrl = mettingUrl;
+
+        console.log("++++++++++++", sessionData);
+
         const newSession = await sessionDb.create(sessionData);
+
+        console.log('++++++++++++++++', newSession);
         
         return newSession;
 
     } catch (error) {
-        throw new Error('Failed to create new Session for chapter');
+        throw new Error('Failed to create a new Session for the chapter');
     }
 };
 
