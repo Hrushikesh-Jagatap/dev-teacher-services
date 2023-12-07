b10c9a1a06ce1b85b5a96d5d67fa397714ad2471
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-07 15:13:49 +0530

    Create index.js

---
+++ src/apis/services/v1/Notification/index.js
@@ -0,0 +1,12 @@
+const { loadBalancer } = require('@config');
+const { RequestHandler } = require('intelli-utility');
+
+const pushNotification = async (data) => {
+    try {
+        const url = `${loadBalancer}/ms/apis/v1/notification/push`;
+        const { data: response } = await RequestHandler.post({ url, data });
+        return response
+    } catch (error) {
+        throw new Error;
+    }
+};
